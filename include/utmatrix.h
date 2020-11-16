9101953fcadc0c2264fb7eca9acc85aab69374f7
Julia <juliadenisova107652@gmail.com>
2020-11-16 18:11:21 +0300

    Ispravleno

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -45,45 +45,39 @@ public:
   ValType  operator*(const TVector &v);     // скалярное произведение
 
   // ввод-вывод
-  friend istream& operator>>(istream &in, TVector &v)
+  friend istream& operator>>(istream& in, TVector& v)
   {
-    for (int i = 0; i < v.Size; i++)
-      in >> v.pVector[i];
-    return in;
+      for (int i = 0; i < v.Size; i++)
+          in >> v.pVector[i];
+      return in;
   }
-  friend ostream& operator<<(ostream &out, const TVector &v)
+  friend ostream& operator<<(ostream& out, const TVector& v)
   {
-    for (int i = 0; i < v.Size; i++)
-      out << v.pVector[i] << ' ';
-    return out;
+      for (int i = 0; i < v.Size; i++)
+          out << v.pVector[i] << ' ';
+      return out;
   }
 };
 
 template <class ValType>
 TVector<ValType>::TVector(int s, int si)
 {
-    if (si<0 || si>MAX_VECTOR_SIZE)
-    {
-        throw si;
-    }
-    if (s > 0 && s < (MAX_VECTOR_SIZE + 1))
+    if ((s < 0) || (si < 0) || (s > MAX_VECTOR_SIZE) || (si > MAX_VECTOR_SIZE))
+        throw "Not available Size or StartIndex";
+    else
     {
         Size = s;
         StartIndex = si;
         pVector = new ValType[Size];
     }
-    else
-    {
-        throw s;
-    }
-}
+} 
 
 template <class ValType> //конструктор копирования
-TVector<ValType>::TVector(const TVector<ValType> &v)
+TVector<ValType>::TVector(const TVector<ValType>& v)
 {
     Size = v.Size;
     StartIndex = v.StartIndex;
-    pVector = new ValType[v.Size];
+    pVector = new ValType[Size];
     for (int i = 0; i < Size; i++)
     {
         pVector[i] = v.pVector[i];
@@ -102,12 +96,12 @@ TVector<ValType>::~TVector()
 template <class ValType> // доступ
 ValType& TVector<ValType>::operator[](int pos)
 {
-    if (pos < 0 || pos >= Size)
+    if ((pos - StartIndex < 0) || (pos > MAX_VECTOR_SIZE) || (pos > StartIndex + Size))
     {
-        throw pos;
+        throw "Out of bounds of the vector";
     }
     return pVector[pos - StartIndex];
-} 
+}
 
 template <class ValType> // сравнение
 bool TVector<ValType>::operator==(const TVector &v) const
@@ -266,37 +260,35 @@ public:
   TMatrix  operator- (const TMatrix &mt);        // вычитание
 
   // ввод / вывод
-  friend istream& operator>>(istream &in, TMatrix &mt)
+  friend istream& operator>>(istream& in, TMatrix& mt)
   {
-    for (int i = 0; i < mt.Size; i++)
-      in >> mt.pVector[i];
-    return in;
+      for (int i = 0; i < mt.Size; i++)
+          in >> mt.pVector[i];
+      return in;
   }
-  friend ostream & operator<<( ostream &out, const TMatrix &mt)
+  friend ostream& operator<<(ostream& out, const TMatrix& mt)
   {
-    for (int i = 0; i < mt.Size; i++)
-      out << mt.pVector[i] << endl;
-    return out;
+      for (int i = 0; i < mt.Size; i++)
+          out << mt.pVector[i] << endl;
+      return out;
   }
 };
 
 template <class ValType>
-TMatrix<ValType>::TMatrix(int s): TVector<TVector<ValType> >(s)
+TMatrix<ValType>::TMatrix(int s = 10) : TVector<TVector<ValType> >(s)
 {
-    if (s<0 || s>MAX_MATRIX_SIZE)
+    if (s <= 0 || s > MAX_MATRIX_SIZE)
     {
         throw s;
     }
-    else
+    Size = s;
+    StartIndex = 0;
+    for (int i = 0; i < s; i++)
     {
-        Size = s;
-        for (int i = 0; i < Size; i++)
-        {
-            TVector<ValType> tmp(Size, i);
-            pVector[i] = tmp;
-        }
+        TVector <ValType> temp(s - i, i);
+        pVector[i] = temp;
     }
-} 
+}
 
 template <class ValType> // конструктор копирования
 TMatrix<ValType>::TMatrix(const TMatrix<ValType>& mt) :
@@ -372,4 +364,5 @@ TMatrix<ValType> TMatrix<ValType>::operator-(const TMatrix<ValType> &mt)
 
 // TVector О3 Л2 П4 С6
 // TMatrix О2 Л2 П3 С3
+
 #endif
